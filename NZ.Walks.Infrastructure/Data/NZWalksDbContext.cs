c258a6e33a20822fcf3f553e8458036a00bf8a7a
BeRo <beroooberooo6@gmail.com>
2025-06-04 17:56:01 +0300

    Add DbContext file

---
+++ NZ.Walks.Infrastructure/Data/NZWalksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NZ.Walks.Domain.Entities;
+
+namespace NZ.Walks.Infrastructure.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
