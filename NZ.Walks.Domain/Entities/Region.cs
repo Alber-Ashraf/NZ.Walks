5e66174bd056140523cbf45101eef40c1b6b744a
BeRo <beroooberooo6@gmail.com>
2025-06-04 17:06:39 +0300

    Add Entities "Walk - Difficulty - Region"

---
+++ NZ.Walks.Domain/Entities/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZ.Walks.Domain.Entities
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        private string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
---
+++ NZ.Walks.Domain/Entities/Walk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZ.Walks.Domain.Entities
+{
+    public class Walk
+    {
+        private Guid Id { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        // Foreign keys
+        private Guid DifficultyId { get; set; }
+        private Guid RegionId { get; set; }
+
+        // Navigation properties
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
